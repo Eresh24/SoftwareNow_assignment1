a851e7f8f75c297a8c75aa156586ba0d97eefd1d
Oshan Thapa Chhetri <oshan9814158662@gmail.com>
2025-08-04 16:37:47 +1000

    oshan

---
+++ assignment1_task1.py
@@ -0,0 +1,21 @@
+'''
+Ask the user yo inout three number and check if these numbers can form a triangle.
+-> Need three inputs
+-> The output shows whether the result can form a triangle or not
+'''
+
+#Take sides from user
+
+side1=float(input("Enter first side:")) 
+side2=float(input("Enter second side:"))
+side3=float( input("Enter third side:"))
+
+# by default sorts the input in asceding order
+sides = sorted([side1,side2,side3])
+
+# Now calculate if sum of smaller two sides > largest side
+
+if sides[0] + sides[1] > sides[2]:
+    print("Yes, they can form triangle")
+else:
+    print("NO, they cannot form triangle")
